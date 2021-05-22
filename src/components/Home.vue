cad1557935c9eddfb0ba9991b56a59042027e7fb
guodayang999 <33561649+guodayang999@users.noreply.github.com>
2021-05-23 07:35:03 +0800

    fen

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,37 @@
 <template>
-  <el-container>
-    <el-main class="login">
-          <el-image :src="require('@/assets/logo.png')" style="height: auto">
-          </el-image>
-    <div style="color: white">---------------------------分割线-------------------------</div>
-    </el-main>
-  </el-container>
+  <el-main>
+    <div style="width: 100%;height: 40.76vw;">
+      <img src="@/assets/home_background.png" style="height: auto;width: 100%;z-index: -1;position:absolute;">
+      <div style="color: violet">2222222222222</div>
+    </div>
+    31231232432
+    <div style="color: green">---------------------------分割线-------------------------</div>
+  </el-main>
+
 </template>
 
 <script>
+import jpg from '@/assets/home_background.png'
+
 export default {
-  name: 'Home'
-  // data: function () {
-  //   return {
-  //     src: ('@/assets/logo.png')
-  //   }
-  // }
+  name: 'Home',
+  data: function () {
+    return {
+      imgUrl: jpg
+    }
+  }
 }
 </script>
 
 <style scoped>
 /* 背景图片*/
-.login{
-  background:url("../assets/home_background.png");
+.login {
+  background: url("../assets/home_background.png");
   width: 100%;
   height: 100%;
-  position:fixed;
-  margin-top: -65px;/*上边距*/
-  margin-left: -8px;/*左边距*/
+  position: fixed;
+  margin-top: -65px; /*上边距*/
+  background-size: 100% 100%;
   z-index: -1;
 }
 </style>
