99eb2a2510cfd12de7bcfeda4f925c96c17aeca1
guodayang999 <33561649+guodayang999@users.noreply.github.com>
2021-05-23 08:42:52 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <!--    <img src="./assets/logo.png">-->
-    <router-view></router-view>
+    <router-view style="padding: 0px;margin: 0px;"></router-view>
 </template>
 
 <script>
@@ -10,12 +10,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin:-1px 0 0 0 ;
+  padding:0;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <el-main>
     <div style="width: 100%;height: 40.76vw;">
-      <img src="@/assets/home_background.png" style="height: auto;width: 100%;z-index: -1;position:absolute;">
+<!--      <img src="@/assets/home_background.png" style="height: auto;width: 100%;z-index: -1;position:absolute;">-->
+      <el-image :src="require('../assets/home_background.png')"
+                style="z-index: -1;position:absolute;"></el-image>
       <div style="color: violet">2222222222222</div>
+      <div style="color: violet;padding: 10px">3333333333333</div>
     </div>
     31231232432
     <div style="color: green">---------------------------分割线-------------------------</div>
@@ -24,14 +27,5 @@ export default {
 </script>
 
 <style scoped>
-/* 背景图片*/
-.login {
-  background: url("../assets/home_background.png");
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  margin-top: -65px; /*上边距*/
-  background-size: 100% 100%;
-  z-index: -1;
-}
+
 </style>
